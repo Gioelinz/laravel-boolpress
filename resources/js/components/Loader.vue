1331b50b6a17216697d5f9b8aa29d6fe26d53c49
Gioelinz <gioelerunza.gr@gmail.com>
2022-04-04 17:43:47 +0200

    better loader

--- resources/js/components/Loader.vue
+++ resources/js/components/Loader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="spinner-grow text-primary" role="status">
-    <span class="sr-only">Loading...</span>
+  <div class="loader">
+    <div class="d-flex align-items-center">
+      <strong class="text-danger mx-3 h3">Posts Loading</strong>
+      <div
+        class="spinner-border text-danger h4 ms-auto"
+        role="status"
+        aria-hidden="true"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +17,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.loader {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(rgb(0, 0, 0), 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .spinner-border {
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+}
 </style>
